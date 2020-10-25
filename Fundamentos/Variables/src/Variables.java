7aa76bc0a1608790e733295184d815919d95d8e2
jcandia <josecandiap@gmail.com>
2020-10-24 21:23:21 -0300

    Definicion de variables

---
+++ Fundamentos/Variables/src/Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    
+    public static void main(String[] args) {
+   
+        //Se define la variable inicial
+        int miVariableEntera = 10;
+        
+        System.out.println(miVariableEntera);
+        
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        
+        System.out.println(miVariableEntera);
+        
+        //Se define la variable de cadena
+        String miVariableCadena = "Saludos";
+        
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adios";
+        
+        System.out.println(miVariableCadena);
+    }
+    
+}
